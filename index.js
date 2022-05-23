f4f73b6ea288d42446d9bcfd7fa62822ab66bc2e
Saikat048 <mdsaikathosan048@gmail.com>
2022-05-24 04:36:46 +0600

    commit tools find one

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
+const cors = require('cors');
 const port = process.env.PORT || 5000
 
 const uri = `mongodb+srv://${process.env.DB_ASSIGNMENT}:${process.env.DB_PASSWORD}@cluster0.a6obg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+app.use(cors());
+app.use(express.json());
 
 
 async function run() {
@@ -21,6 +27,15 @@ async function run() {
         const tools = await cursor.toArray();
         res.send(tools) 
       });
+
+
+      app.get('/tools/:id', async(req, res) => {
+        const id = req.params.id; 
+        const query = {_id: ObjectId(id)};
+        const tools = await toolsCollection.findOne(query);
+        res.send(tools) 
+      })
+
   
     } finally { 
     //   await client.close(); 
